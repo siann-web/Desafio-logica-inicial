2337aa6f06572eb08ce4ce69d7a0d515b2a96e1f
siann-web <ruanbruno301@gmail.com>
2024-04-22 15:47:36 -0300

    código finalizado

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+let Name = "Ruan";
+let Xp = 5100;
+
+if (Xp <= 1000) {
+  console.log(`${Name} está no Ferro`);
+    } else if (Xp <= 2000 && Xp > 1000) {
+    console.log(`${Name} está no Bronze`);
+        } else if (Xp <= 5000 && Xp > 2000) {
+        console.log(`${Name} está no Prata`);
+            } else if (Xp <= 7000 && Xp > 5000) {
+            console.log(`${Name} está no Ouro`);
+                } else if (Xp <= 8000 && Xp > 7000) {
+                console.log(`${Name} está no Platina`);
+                    } else if (Xp <= 9000 && Xp > 8000) {
+                    console.log(`${Name} está no Ascendente`);
+                        } else if (Xp <= 10000 && Xp > 9000) {
+                        console.log(`${Name} está no Imortal`);
+                            } else { console.log(`${Name} está no Radiante`);}
+console.log(Xp);
